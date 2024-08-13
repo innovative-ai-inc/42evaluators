864348b0f8deffb9454cde6cdda0ea459b48cc3c
Shigurex <yamatohokari@gmail.com>
2024-08-13 18:06:25 +0900

    added deleted parts

--- go.mod
+++ go.mod
@@ -3,8 +3,8 @@ module github.com/demostanis/42evaluators
 go 1.21.3
 
 require (
-	github.com/PuerkitoBio/goquery v1.9.1
-	github.com/a-h/templ v0.2.663
+	github.com/PuerkitoBio/goquery v1.8.1
+	github.com/a-h/templ v0.2.747
 	github.com/bogdanfinn/fhttp v0.5.28
 	github.com/bogdanfinn/tls-client v1.7.4
 	github.com/go-co-op/gocron/v2 v2.2.9
@@ -12,18 +12,15 @@ require (
 	github.com/joho/godotenv v1.5.1
 	golang.org/x/sync v0.7.0
 	golang.org/x/time v0.5.0
-	gorm.io/driver/mysql v1.5.6
 	gorm.io/driver/postgres v1.5.7
 	gorm.io/gorm v1.25.9
 )
 
 require (
-	filippo.io/edwards25519 v1.1.0 // indirect
 	github.com/andybalholm/brotli v1.1.0 // indirect
-	github.com/andybalholm/cascadia v1.3.2 // indirect
+	github.com/andybalholm/cascadia v1.3.1 // indirect
 	github.com/bogdanfinn/utls v1.6.1 // indirect
 	github.com/cloudflare/circl v1.3.7 // indirect
-	github.com/go-sql-driver/mysql v1.8.1 // indirect
 	github.com/google/uuid v1.6.0 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
@@ -38,6 +35,6 @@ require (
 	golang.org/x/crypto v0.22.0 // indirect
 	golang.org/x/exp v0.0.0-20240409090435-93d18d7e34b8 // indirect
 	golang.org/x/net v0.24.0 // indirect
-	golang.org/x/sys v0.19.0 // indirect
+	golang.org/x/sys v0.21.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
 )
--- internal/api/keys.go
+++ internal/api/keys.go
@@ -1,18 +1,27 @@
 package api
 
 import (
+	"bytes"
 	"context"
+	"encoding/json"
 	"errors"
 	"fmt"
+	"io"
+	"mime/multipart"
+	"net/url"
 	"os"
 	"strconv"
+	"strings"
 	"sync"
 
 	"golang.org/x/sync/semaphore"
 
+	"github.com/PuerkitoBio/goquery"
+	http "github.com/bogdanfinn/fhttp"
 	tls_client "github.com/bogdanfinn/tls-client"
 	"github.com/bogdanfinn/tls-client/profiles"
 	"github.com/demostanis/42evaluators/internal/models"
+	"github.com/joho/godotenv"
 	"gorm.io/gorm"
 )
 
@@ -21,7 +30,7 @@ const (
 	concurrentFetches = int64(42)
 	defaultUserAgent  = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
 	defaultAppName    = "42evaluators"
-	keysToGenerate    = 400
+	keysToGenerate    = 5
 )
 
 var defaultRedirectURI = "http://localhost:8080"
@@ -103,7 +112,57 @@ func (manager *KeysManager) GetKeys() ([]models.APIKey, error) {
 }
 
 func (manager *KeysManager) CreateOne() (*models.APIKey, error) {
-	panic("deleted, sorry ;(")
+	if err := godotenv.Load(); err != nil {
+		return nil, err
+	}
+
+	buf, writer, err := manager.buildForm(defaultAppName)
+	if err != nil {
+		return nil, err
+	}
+
+	req, _ := http.NewRequest("POST",
+		fmt.Sprintf("https://%s/oauth/applications", host),
+		io.NopCloser(buf))
+	req.Header = http.Header{
+		"Cookie": {fmt.Sprintf("user.id=%s; _intra_42_session_production=%s",
+			manager.userIDToken, manager.intraSessionToken)},
+		"Origin":       {"https://profile.intra.42.fr"},
+		"Referer":      {"https://profile.intra.42.fr/oauth/applications/new"},
+		"User-Agent":   {defaultUserAgent},
+		"Content-Type": {writer.FormDataContentType()},
+	}
+
+	resp, err := manager.client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	doc, err := goquery.NewDocumentFromReader(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	apiKey := &models.APIKey{Name: defaultAppName}
+	elems := strings.Split(doc.Find("a[href^='/oauth/applications/']").AttrOr("href", ""), "/")
+	if len(elems) < 4 {
+		return nil, errors.New("invalid response, the authenticity token is wrong?")
+	}
+	appIDraw := elems[3]
+	if appIDraw == "" {
+		return nil, errors.New("error could not find the application ID in html body")
+	}
+	appID, err := strconv.Atoi(appIDraw)
+	if err != nil {
+		return nil, err
+	}
+	apiKey.AppID = appID
+
+	if err = manager.fetchAPIData(apiKey); err != nil {
+		return nil, err
+	}
+	return apiKey, manager.db.Create(apiKey).Error
 }
 
 func (manager *KeysManager) createMany(n int) ([]models.APIKey, error) {
@@ -144,11 +203,78 @@ func (manager *KeysManager) createMany(n int) ([]models.APIKey, error) {
 }
 
 func (manager *KeysManager) pullAuthenticityToken() error {
-	panic("deleted, sorry ;(")
+	req, _ := http.NewRequest("GET",
+		fmt.Sprintf("https://%s/oauth/applications", host), nil)
+	req.Header = http.Header{
+		"Cookie": {fmt.Sprintf("user.id=%s; _intra_42_session_production=%s",
+			manager.userIDToken, manager.intraSessionToken)},
+		"Referer":    {"https://profile.intra.42.fr/languages"},
+		"User-Agent": {defaultUserAgent},
+	}
+
+	resp, err := manager.client.Do(req)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	doc, err := goquery.NewDocumentFromReader(resp.Body)
+	if err != nil {
+		return err
+	}
+
+	println(doc.Find("meta[name=csrf-token]"))
+
+	var ok bool
+	manager.authToken, ok = doc.Find("meta[name=csrf-token]").Attr("content")
+	// TODO: invalid response, expired credentials?
+	if !ok {
+		return fmt.Errorf("invalid response, expired credentials?")
+	}
+
+	return nil
 }
 
 func (manager *KeysManager) fetchAPIKeysFromIntra() ([]string, error) {
-	panic("deleted, sorry ;(")
+	req, _ := http.NewRequest("GET",
+		fmt.Sprintf("https://%s/oauth/applications", host), nil)
+	req.Header = http.Header{
+		"Cookie": {fmt.Sprintf("user.id=%s; _intra_42_session_production=%s",
+			manager.userIDToken, manager.intraSessionToken)},
+		"Origin":     {"https://profile.intra.42.fr"},
+		"Referer":    {"https://profile.intra.42.fr/oauth/applications/new"},
+		"User-Agent": {defaultUserAgent},
+	}
+
+	resp, err := manager.client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	doc, err := goquery.NewDocumentFromReader(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	result := make([]string, 0)
+
+	data, ok := doc.Find(".apps-root").First().Attr("data")
+	if !ok {
+		return nil, errors.New("no .apps-root")
+	}
+
+	var apps []struct{ ID int }
+	err = json.Unmarshal([]byte(data), &apps)
+	if err != nil {
+		return nil, err
+	}
+
+	for _, app := range apps {
+		result = append(result, strconv.Itoa(app.ID))
+	}
+
+	return result, nil
 }
 
 func (manager *KeysManager) DeleteAllKeys() error {
@@ -188,5 +314,96 @@ func (manager *KeysManager) DeleteAllKeys() error {
 }
 
 func (manager *KeysManager) DeleteOne(id int) error {
-	panic("deleted, sorry ;(")
+	req, _ := http.NewRequest("POST",
+		fmt.Sprintf("https://%s/oauth/applications/%d", host, id),
+		io.NopCloser(
+			bytes.NewReader(
+				[]byte("_method=delete&authenticity_token="+
+					url.QueryEscape(manager.authToken)))))
+
+	req.Header = http.Header{
+		"Cookie": {fmt.Sprintf("user.id=%s; _intra_42_session_production=%s",
+			manager.userIDToken, manager.intraSessionToken)},
+		"Origin":       {"https://profile.intra.42.fr"},
+		"Referer":      {fmt.Sprintf("https://profile.intra.42.fr/oauth/applications/%d", id)},
+		"User-Agent":   {defaultUserAgent},
+		"Accept":       {"text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8"},
+		"Content-Type": {"application/x-www-form-urlencoded"},
+	}
+
+	resp, err := manager.client.Do(req)
+	if err != nil {
+		return fmt.Errorf("failed to delete application: %w", err)
+	}
+	defer resp.Body.Close()
+
+	return manager.db.
+		Where("id = ?", id).
+		Delete(&models.APIKey{}).Error
+}
+
+func (manager *KeysManager) fetchAPIData(api *models.APIKey) error {
+	req, _ := http.NewRequest("GET",
+		fmt.Sprintf("https://%s/oauth/applications/%d", host, api.AppID), nil)
+	req.Header = http.Header{
+		"Cookie": {fmt.Sprintf("user.id=%s; _intra_42_session_production=%s",
+			manager.userIDToken, manager.intraSessionToken)},
+		"Origin":     {"https://profile.intra.42.fr"},
+		"Referer":    {"https://profile.intra.42.fr/oauth/applications/new"},
+		"User-Agent": {defaultUserAgent},
+	}
+
+	resp, err := manager.client.Do(req)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	doc, err := goquery.NewDocumentFromReader(resp.Body)
+	if err != nil {
+		return err
+	}
+
+	doc.Find(".credential").Each(func(i int, s *goquery.Selection) {
+		switch i {
+		case 0:
+			api.UID = s.Text()
+		case 1:
+			api.Secret = s.Text()
+		}
+	})
+
+	api.RedirectURI = doc.Find(".redirect-uri-block code").First().Text()
+
+	return nil
+}
+
+func (manager *KeysManager) buildForm(appName string) (*bytes.Buffer, *multipart.Writer, error) {
+	buf := &bytes.Buffer{}
+
+	writer := multipart.NewWriter(buf)
+	_ = writer.WriteField("utf8", "✓")
+	_ = writer.WriteField("authenticity_token", manager.authToken)
+	_ = writer.WriteField("doorkeeper_application[name]", appName)
+	_ = writer.WriteField("doorkeeper_application[image_cache]", "")
+
+	// creates an empty file and add it to the form.
+	part, err := writer.CreateFormFile("doorkeeper_application[image]", "")
+	if err != nil {
+		return buf, nil, err
+	}
+	// copy empty bytes to the file.
+	if _, err = io.Copy(part, bytes.NewReader([]byte{})); err != nil {
+		return buf, nil, err
+	}
+
+	_ = writer.WriteField("doorkeeper_application[description]", "")
+	_ = writer.WriteField("doorkeeper_application[website]", "")
+	_ = writer.WriteField("doorkeeper_application[public]", "0")
+	_ = writer.WriteField("doorkeeper_application[scopes]", "")
+	_ = writer.WriteField("doorkeeper_application[redirect_uri]", manager.redirectURI)
+	_ = writer.WriteField("commit", "Submit")
+
+	err = writer.Close()
+	return buf, writer, err
 }
